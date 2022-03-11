b013a125a7fadde3f84bdc4fa0331c85d80b9883
rmuehe <rmuehe@gmail.com>
2022-03-11 13:25:30 -0800

    Fix edit registration pages to update

--- app/controllers/providers/registrations_controller.rb
+++ app/controllers/providers/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Providers::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -15,9 +15,9 @@ class Providers::RegistrationsController < Devise::RegistrationsController
   end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
   # def update
@@ -47,9 +47,10 @@ class Providers::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
+  def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :summary, :phone, :website, :serving, :email])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
